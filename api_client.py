ba9709e243e040f2f71858d488e9beb613ed408d
YasharHND <yashar.hnd.65@gmail.com>
2023-12-09 01:55:20 +0400

    update

--- api_client.py
+++ api_client.py
@@ -15,21 +15,19 @@ printify_base_url = "https://api.printify.com"
 
 def save(content, file_name):
     with open(f"Saves/{file_name}.json", "w") as file:
-        file.write(content)
+        file.write(json.dumps(content, indent=2))
 
 
 def get_and_save(uri, file_name):
     response = requests.get(f"{printify_base_url}{uri}", headers=headers)
-    content = json.loads(response.content)
-    content_json = json.dumps(content, indent=2)
-    save(content_json, file_name)
+    content = response.json()
+    save(content, file_name)
 
 
 def post_and_save(uri, body, file_name):
     response = requests.post(f"{printify_base_url}{uri}", headers=headers, data=json.dumps(body))
-    content = json.loads(response.content)
-    content_json = json.dumps(content, indent=2)
-    save(content_json, file_name)
+    content = response.json()
+    save(content, file_name)
     return content
 
 
