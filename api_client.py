d9d769709f720a8e82b351d0767052b49a99e91e
YasharHND <yashar.hnd.65@gmail.com>
2023-12-08 17:36:15 +0400

    working

--- api_client.py
+++ api_client.py
@@ -7,25 +7,31 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv("PRINTIFY_TOKEN")
 headers = {
-    "Authorization": f"Bearer {token}"
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json"
 }
 printify_base_url = "https://api.printify.com"
 
 
-def save(response, file_name):
-    content = json.loads(response.content)
+def save(content, file_name):
     with open(f"Saves/{file_name}.json", "w") as file:
-        file.write(json.dumps(content, indent=2))
+        file.write(content)
 
 
 def get_and_save(uri, file_name):
     response = requests.get(f"{printify_base_url}{uri}", headers=headers)
-    save(response, file_name)
+    content = json.loads(response.content)
+    content_json = json.dumps(content, indent=2)
+    save(content_json, file_name)
 
 
 def post_and_save(uri, body, file_name):
-    print(json.dumps(body, indent=2))
     response = requests.post(f"{printify_base_url}{uri}", headers=headers, data=json.dumps(body))
-    print(response.status_code)
-    print(response.content)
-    save(response, file_name)
+    content = json.loads(response.content)
+    content_json = json.dumps(content, indent=2)
+    save(content_json, file_name)
+    return content
+
+
+def post(uri, body):
+    requests.post(f"{printify_base_url}{uri}", headers=headers, data=json.dumps(body))
